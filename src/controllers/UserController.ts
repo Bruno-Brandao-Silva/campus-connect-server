ba4c90a64f34fedf99d07d6f2333b16c89dfd7b6
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-11-16 23:46:10 -0300

    User improvements

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,92 +4,83 @@ import axios from 'axios';
 import bcrypt from 'bcrypt';
 import { signToken } from '../middlewares/AuthMiddleware';
 
-const UTFPR_BASE_URI = process.env.UTFPR_BASE_URI!; // URL base da API Portal do Aluno
+const UTFPR_BASE_URI = process.env.UTFPR_BASE_URI!;
 
 const UserController = {
 	login: async (req: Request, res: Response) => {
 		try {
 			const { academicRegistration, password } = req.body;
 			const hashedPassword = await bcrypt.hash(password, 10);
-			const user = await User.find({ academicRegistration });
-			if (user.length === 0) {
-				console.log("Usuário não existe, fazendo requisição para o Portal do Aluno");
-				const { data: login_data } = await axios.post(`${UTFPR_BASE_URI}/auth`, { username: academicRegistration, password });
-				const { data: user_data } = await axios.get(`${UTFPR_BASE_URI}/dados`, { headers: { Authorization: `Bearer ${login_data.token}` } });
-				const { data: profilePicture } = await axios.get(`${UTFPR_BASE_URI}/fotoCracha`, { headers: { Authorization: `Bearer ${login_data.token}` } });
+			const user = await User.findOne({ academicRegistration });
+
+			if (!user) {
+				console.log("User does not exist, making a request to the Student Portal");
+				const { data: loginData } = await axios.post(`${UTFPR_BASE_URI}/auth`, { username: academicRegistration, password });
+				const { data: userData } = await axios.get(`${UTFPR_BASE_URI}/dados`, { headers: { Authorization: `Bearer ${loginData.token}` } });
+				const { data: profilePicture } = await axios.get(`${UTFPR_BASE_URI}/fotoCracha`, { headers: { Authorization: `Bearer ${loginData.token}` } });
 				const profileBuffer = Buffer.from(profilePicture, 'base64');
 
-				const { year, period } = user_data.cursos.map((curso: any) => { return { year: curso.alCuAnoingNr, period: curso.alCuPerAnoingNr } }).reduce((prev: any, curr: any) => {
-					if (prev.year > curr.year) {
-						return prev;
-					} else if (prev.year < curr.year) {
-						return curr;
-					} else {
-						if (prev.period > curr.period) {
-							return prev;
-						} else {
-							return curr;
-						}
-					}
-				});
-				await User.create({
+				const { year, period } = userData.cursos.reduce((prev: any, curr: any) => {
+					return (prev.year > curr.alCuAnoingNr || (prev.year === curr.alCuAnoingNr && prev.period >= curr.alCuPerAnoingNr)) ? prev : curr;
+				}, {});
+
+				const newUser = await User.create({
 					academicRegistration,
 					password: hashedPassword,
-					fullName: (user_data.pessNomeVc as string),
+					fullName: userData.pessNomeVc as string,
 					profilePicture: profileBuffer,
-					academicSchedule: user_data.cursos.map((curso: any) => curso.cursNomeVc),
+					academicSchedule: userData.cursos.map((curso: any) => curso.cursNomeVc),
 					entryYear: year,
 					entryPeriod: period,
 				});
-				const user = await User.find({ academicRegistration });
-				console.log(user[0]);
-				const token = signToken(user[0]._id);
+
+				console.log(newUser);
+				const token = signToken(newUser._id);
 				res.cookie('CCS-AUTH-TOKEN', token);
-				return res.json({ statusText: "Usuário cadastrado com sucesso", token, firstAcess: true });
+				return res.json({ statusText: "User registered successfully", token, firstAccess: true });
 			} else {
-				const token = signToken(user[0]._id);
-				console.log(user[0]);
+				const token = signToken(user._id);
+				console.log(user);
 				res.cookie('CCS-AUTH-TOKEN', token);
-				return res.json({ statusText: "Usuário  já existe", token, firstAcess: false });
+				return res.json({ statusText: "User already exists", token, firstAccess: false });
 			}
 		} catch (error: any) {
 			console.error(error);
-			return res.status(error.response.status).json({ message: error.message });
+			return res.status(error.response?.status || 500).json({ message: error.message });
 		}
 	},
+
 	getDataById: async (req: Request, res: Response) => {
 		try {
 			const userId = req.userId;
-			const users = await User.find({ _id: userId });
-			res.json(users);
+			const user = await User.findById(userId);
+			res.json(user);
 		} catch (error) {
 			console.error(error);
-			res.status(500).json({ message: 'Error getting users' });
+			res.status(500).json({ message: 'Error getting user data' });
 		}
 	},
+
 	getPublicData: async (req: Request, res: Response) => {
 		try {
-			const users = await User.find();
-			// Remove os campos que não devem ser públicos
+			const users = await User.find({}, { academicRegistration: 1, fullName: 1, academicSchedule: 1, entryYear: 1, entryPeriod: 1 });
 			res.json(users);
 		} catch (error) {
 			console.error(error);
-			res.status(500).json({ message: 'Error getting users' });
+			res.status(500).json({ message: 'Error getting public user data' });
 		}
 	},
+
 	getPublicDataById: async (req: Request, res: Response) => {
 		try {
 			const userId = req.params.id;
-			const users = await User.find({ _id: userId });
-			// Remove os campos que não devem ser públicos
-
-			res.json(users);
+			const user = await User.findById(userId, { academicRegistration: 1, fullName: 1, academicSchedule: 1, entryYear: 1, entryPeriod: 1 });
+			res.json(user);
 		} catch (error) {
 			console.error(error);
-			res.status(500).json({ message: 'Error getting users' });
+			res.status(500).json({ message: 'Error getting public user data' });
 		}
 	},
-
 
 	edit: async (req: Request, res: Response) => {
 		try {
@@ -102,9 +93,9 @@ const UserController = {
 			res.status(500).json({ message: 'Error editing profile' });
 		}
 	},
+
 	delete: async (req: Request, res: Response) => {
 		try {
-			// const userId = req.userId;
 			const userId = req.params.id;
 			await User.findByIdAndDelete(userId);
 			res.json({ message: 'User deleted successfully' });
