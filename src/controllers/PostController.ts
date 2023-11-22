105b3aa812f43718c875d456c75d33798a9b98b1
Bruno Brandão Silva <brunowbrandao@gmail.com>
2023-11-22 18:34:09 -0300

    Refactor getMyPosts method to include author information

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -18,9 +18,21 @@ const PostController = {
   getMyPosts: async (req: Request, res: Response) => {
     try {
       const { _id } = req.UserJwtPayload;
+      const user = await User.findById(_id);
       const posts = await Post.find({ author: _id }).sort({ createdAt: -1 }).limit(10);
-
-      res.json(posts);
+      const postsWithAuthor = posts.map((post) => {
+        return {
+          id: post._id,
+          avatar: user.profilePicture,
+          username: user.name,
+          userAt: user.username,
+          createdAt: post.createdAt,
+          universityPeriod: user.entryBadge,
+          description: post.title,
+          file: post.mediaId
+        }
+      })
+      res.json(postsWithAuthor);
     } catch (error) {
       res.status(500).json({ error: 'Error fetching posts' });
     }
